33c8ba8d25677cbbbd9bbb0ebdaf8afc766d60a2
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-19 23:10:52 -0300

    chore: Refactor firstLetterUpperCase function in strings-utils.ts

--- utils/strings-utils.ts
+++ utils/strings-utils.ts
@@ -1,7 +1,3 @@
 export const firstLetterUpperCase = (str: string) => {
-  if (str) {
-    return str.charAt(0).toLocaleUpperCase() + str.slice(1);
-  } else {
-    return "";
-  }
+  return str ? str.charAt(0).toLocaleUpperCase() + str.slice(1) : "";
 };
